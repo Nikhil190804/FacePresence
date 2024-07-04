adf93d6454b38d697d65ad5cb0fa3e21e58d5d06
Nikhil <nikhil22322@iiitd.ac.in>
2024-07-04 23:08:40 +0530

    INITIALIZED A PYTHON PROJECT WITH VIRTUAL ENV

---
+++ run.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__,template_folder='app/templates', static_folder='app/static')
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return render_template("./login.html")
+
+# main driver function
+if __name__ == '__main__':
+    app.run(debug=True)
